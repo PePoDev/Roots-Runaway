34df0a12e28cdec42da9536f3235a4d830b440b1
Thiwanon Chomcharoen <gmgame.1525@gmail.com>
2023-02-03 21:19:35 +0700

    init: add plugins and assets

---
+++ Assets/Plugins/Ludiq/Ludiq.Peek/Editor/Plugin/Changelogs/Changelog_1_4_1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using Ludiq.Peek;
+using Ludiq.PeekCore;
+
+[assembly: MapToPlugin(typeof(Changelog_1_4_1), PeekPlugin.ID)]
+
+namespace Ludiq.Peek
+{
+	// ReSharper disable once RedundantUsingDirective
+	using PeekCore;
+
+	internal class Changelog_1_4_1 : PluginChangelog
+	{
+		public Changelog_1_4_1(Plugin plugin) : base(plugin) { }
+
+		public override SemanticVersion version => "1.4.1";
+
+		public override DateTime date => new DateTime(2021, 11, 09);
+
+		public override IEnumerable<string> changes
+		{
+			get
+			{
+				yield return "[Fixed] Compatibility with Unity 2021.1 and 2021.2";
+			}
+		}
+	}
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+	public float speed;
+    
+	private void Start()
+	{
+        
+	}
+    
+	private void Update()
+	{
+        
+	}
+    }
